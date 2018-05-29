b367371d1e714c10267ba8e512b17a3f25e68d97
mmadGit49 <mohammadkim49@gmail.com>
2018-05-29 22:43:53 +0300

    Navigation bar added TODO: Edit bar

--- app/src/main/java/com/example/mohammad/studentpa/MainActivity.java
+++ app/src/main/java/com/example/mohammad/studentpa/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.mohammad.studentpa;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    /**TODO: Check and verify navigation menu button functionality
+     * TODO: Add visual appeal to menu items
+     * TODO: Comment on how onOptionsItemSelect method works
+     * 
+     *
+    */
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+
+        drawerLayout.addDrawerListener(toggle);//the listener for the menu button
+        toggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){//method to activate the clicked menu button
+        if(toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
